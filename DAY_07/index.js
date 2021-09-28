c84fee57c0e7b7320af1c85030cd5d1af0c99c69
arpit gupta <mycontact.arpit@gmail.com>
2021-09-28 21:36:35 +0530

    done

---
+++ DAY_07/index.js
@@ -0,0 +1,25 @@
+const secondHand= document.querySelector(".second-hand");
+const minsHand= document.querySelector('.min-hand');
+const hourHand =document.querySelector('.hour-hand');
+
+function setDate(){
+
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds/60)*360)+90;
+    secondHand.style.transform =`rotate(${secondDegrees}deg)`;
+
+
+    const mins= now.getMinutes();
+    const minsDegrees =((mins/60)*360)+90;
+    minsHand.style.transform=`rotate(${minsDegrees}deg)`;
+
+    const hour= now.getMinutes();
+    const hourDegrees =((mins/12)*360)+90;
+    hourHand.style.transform=`rotate(${hourDegrees}deg)`;
+
+    console.log(seconds);
+
+}
+
+setInterval(setDate,1000)

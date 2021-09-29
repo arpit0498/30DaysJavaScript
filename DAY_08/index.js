2c2eee09d70579df1ddc1a6afd88b954666ca7fa
arpit gupta <mycontact.arpit@gmail.com>
2021-09-29 14:00:16 +0530

    images added

---
+++ DAY_08/index.js
@@ -0,0 +1,4 @@
+    const gallery = document.querySelector('.gallery');
+    const overlay = document.querySelector('.overlay');
+    const overlayImage = overlay.querySelector('img');
+    const overlayClose = overlay.querySelector('.close');

d8c52de7e5d9a2ac3c6ca85cb00f6c2f5480841c
arpit gupta <mycontact.arpit@gmail.com>
2021-09-29 15:24:07 +0530

    refactor

--- DAY_08/index.js
+++ DAY_08/index.js
@@ -1,28 +1,62 @@
-   const gallery = document.querySelector('.gallery');
-    const overlay = document.querySelector('.overlay');
-    const overlayImage = overlay.querySelector('img');
-    const overlayClose = overlay.querySelector('.close');
+  const gallery = document.querySelector('.gallery');
+  const overlay = document.querySelector('.overlay');
+  const overlayImage = overlay.querySelector('img');
+  const overlayClose = overlay.querySelector('.close');
+
+  function generateHTML([h, v]) {
+    return `
+        <div class="item h${h} v${v}">
+          <img src="images/${randomNumber(12)}.jpg">
+          <div class="item__overlay">
+            <button>View →</button>
+          </div>
+        </div>
+      `;
+  }
+
+  function randomNumber(limit) {
+    return Math.floor(Math.random() * limit) + 1;
+  }
+
+  function handleClick(e) {
+    const src = e.currentTarget.querySelector('img').src;
+    overlayImage.src = src;
+    overlay.classList.add('open');
+  }
 
 
+  function close() {
+    overlay.classList.remove('open');
+  }
 
-    function randomNumber(limit){
-        return Math.floor(Math.random() * limit)+1;
-    }
+  const digits = Array.from({
+    length: 50
+  }, () => [randomNumber(4), randomNumber(4)]).concat([
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1]
+  ])
 
-    const digits = Array.from({length:50}, ()=>[randomNumber(4), randomNumber(4)]).concat([[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]);
-    
+  const html = digits.map(generateHTML).join('');
+  gallery.innerHTML = html;
 
-    const html = digits.map(generateHTML).join('');
-    gallery.innerHTML=html;
-    console.log(html);
+  const items = document.querySelectorAll('.item');
 
+  items.forEach(item => item.addEventListener('click', handleClick));
 
-        function generateHTML([h,v]){
-        return`
-        <div class ="item h${h} v ${v}">
-            <img src ="images/${randomNumber(12)}.jpg">
-                <div class="item__overlay">
-                    <button>view 🎫</button>
-                </div>
-        </div>`;
-    }
+  overlayClose.addEventListener('click', close);

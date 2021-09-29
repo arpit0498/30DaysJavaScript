4b1c5ed3259f34de06a7bd147c19f6c7d7ef2740
arpit gupta <mycontact.arpit@gmail.com>
2021-09-29 14:05:30 +0530

    feat:function generate

--- DAY_08/index.js
+++ DAY_08/index.js
@@ -1,4 +1,14 @@
-    const gallery = document.querySelector('.gallery');
+d    const gallery = document.querySelector('.gallery');
     const overlay = document.querySelector('.overlay');
     const overlayImage = overlay.querySelector('img');
     const overlayClose = overlay.querySelector('.close');
+
+    function generateHTML([h,v]){
+        return`
+        <div class ="item h${h} v ${v}>
+            <img src ="images/1.jpg">
+                <div class="item__overlay">
+                    <button>view 🎫</button>
+                </div>
+        </div>`;
+    }
